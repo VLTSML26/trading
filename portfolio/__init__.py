8254108a224b0fbb977a6c4e129d47abc9481b70
Samuele <samuele.voltan@popso.it>
2025-11-27 17:34:13 +0100

    Garbage disposal 2

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -2,14 +2,12 @@ from .portfolio import (
     Portfolio,
     Tickers,
     efficient_frontier,
-    plot_efficient_frontier,
-    plot_ef_cml
+    plot_efficient_frontier
 )
 
 __all__ = [
     "Portfolio",
     "Tickers",
     "efficient_frontier",
-    "plot_efficient_frontier",
-    "plot_ef_cml",
+    "plot_efficient_frontier"
 ]
