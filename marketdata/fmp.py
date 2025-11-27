275a692d9bc729cc2fa90959fe2a39b8276d4819
Samuele <samuele.voltan@popso.it>
2025-11-27 17:01:43 +0100

    FMP: commenti e modifiche minori (tipo periodo = max)

--- marketdata/fmp.py
+++ marketdata/fmp.py
@@ -3,10 +3,14 @@ from dotenv import load_dotenv
 import pandas as pd
 import requests
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from .base import MarketDataProvider
 
 load_dotenv() # carica variabili d'ambiente da .env
 
+# TODO 1: implementare download asincroni
+# TODO 2: implementare download anche di prezzi di apertura, intraday (abbonamento free non so...)
+# TODO 3: scrivere funzioni di download che prendono anche date di inizio e fine pre-definite (attualmente fine = oggi)
 class FMPProvider(MarketDataProvider):
     """
     Provider per Financial Modeling Prep (FMP).
@@ -14,35 +18,38 @@ class FMPProvider(MarketDataProvider):
     """
 
     BASE_URL = "https://financialmodelingprep.com/stable/historical-price-eod/full"
+    DIVIDEND_ADJ_URL = "https://financialmodelingprep.com/stable/historical-price-eod/dividend-adjusted"
 
-    def __init__(self, api_key: str = None, timeout: float = 5.0, retry: int = 3):
+    def __init__(self, api_key: str=None, timeout: float=5.0, retry: int=3):
         """
-        Parametri
-        ---------
-        api_key : str
-            Chiave API: None di default. In tal caso, legge la variabile d'ambiente FMP_API_KEY.
-            Solleva errore se non trovata.
-        timeout : float
-            Timeout per la richiesta HTTP.
-        retry : int
-            Numero di tentativi in caso di failure.
+        Costruttore di FMPProvider.
+        
+        :param api_key: Chiave API (default=None, viene letta la variabile d'ambiente e solleva errore se non trovata).
+        :type api_key: str
+        :param timeout: Timeout per la richiesta HTTP.
+        :type timeout: float
+        :param retry: Numero di tentativi in caso di failure.
+        :type retry: int
         """
         self._api_key = api_key or os.getenv("FMP_API_KEY")
         if self._api_key is None:
-            raise ValueError(
-                "Missing FMP API key. Set FMP_API_KEY in your .env file "
-                "or pass api_key=\"...\" explicitly."
-            )
+            raise ValueError("Missing FMP API key.")
         self.timeout = timeout
         self.retry = retry
 
-    def download(self, tickers, period):
+    def download(self, tickers: list[str], period: str) -> pd.DataFrame:
         """
         Scarica i prezzi di chiusura per uno o più ticker.
-        Restituisce un DataFrame: colonne = tickers, index = date.
+        
+        :param tickers: Lista di nomi dei titoli.
+        :type tickers: list[str]
+        :param period: Periodo di riferimento dei prezzi dei titoli.
+        :type period: str
+        :return: pandas DataFrame indicizzato con date.
+        :rtype: DataFrame
         """
-        tickers = tickers if isinstance(tickers, list) else [tickers] # assicuro lista
-        start_date = self._period_to_start_date(period) # converto 'period' in giorni
+        # conversione di period in una data di inizio
+        start_date = self._period_to_start_date(period)
 
         # scarico ogni ticker singolarmente
         all_closes = {}
@@ -53,35 +60,38 @@ class FMPProvider(MarketDataProvider):
         df = pd.concat(all_closes, axis=1) # combino in un unico DataFrame
         return df.sort_index()
 
-    def _download_single(self, ticker, start_date):
+    def _download_single(self, ticker: str, start_date: datetime | None) -> pd.Series:
         """
-        Scarica i dati storici per un singolo ticker da FMP.
-        Restituisce una Series con i prezzi di chiusura.
+        Download tramite chiamata API dei dati di mercato di un singolo ticker dal provider.
+        
+        :param ticker: Ticker richeisto.
+        :type ticker: str
+        :param start_date: Data di inizio serie storica richiesta.
+        :type start_date: datetime | None
+        :return: Prezzi di chiusura del ticker richiesto.
+        :rtype: Series
         """
         params = {
-            "symbol": ticker.upper(),
+            "symbol": ticker,
             "apikey": self._api_key,
-            "from": start_date.strftime("%Y-%m-%d")#,
-            # "to": datetime.today().strftime("%Y-%m-%d")
+            "from": start_date.strftime("%Y-%m-%d") if start_date else None,
+            "to": None # TODO 3: implementare qua
         }
 
         # tentativi di download dei dati con logiche di retry e timeout
         for _ in range(self.retry):
             try:
-                # effettuo richiesta
-                r = requests.get(self.BASE_URL, params=params, timeout=self.timeout)
-
+                r = requests.get(self.DIVIDEND_ADJ_URL, params=params, timeout=self.timeout)
                 # risposta in formato json
                 if r.status_code != 200:
-                    continue # esco dal ciclo for (considerato come un tentativo fallito)
+                    continue # esco dal for (considerato come un tentativo fallito)
                 js = r.json()
-
                 # converto json in DataFrame e indicizzo datetime
                 df = pd.DataFrame(js)
                 df["date"] = pd.to_datetime(df["date"])
                 df.set_index("date", inplace=True)
 
-                # TODO: restituire tutti i prezzi nella logica di sostituire la classe Tickers
+                # TODO 2: implementare qua
                 return df["close"].rename(ticker)
 
             except Exception:
@@ -89,38 +99,31 @@ class FMPProvider(MarketDataProvider):
 
         raise RuntimeError(f"Download failure for ticker {ticker} from FMP.")
 
-    # FIXME: metodo vecchio non conta bene i giorni
-    def _period_to_days(self, period: str) -> int:
+    def _period_to_start_date(self, period: str) -> datetime | None:
         """
-        Converte '1y', '6mo', '5y', '3d', etc. in un numero di giorni.
-        """
-        period = period.lower()
-
-        if "y" in period:
-            return int(period.replace("y", "")) * 365
-        elif "mo" in period:
-            return int(period.replace("mo", "")) * 30
-        elif "d" in period:
-            return int(period.replace("d", ""))
-        else:
-            raise ValueError(f"Unrecognized period format: {period}")
+        Converte un periodo ('1y', '6mo', '5y', '3d', etc.) nella data di inizio.
         
-    
-    def _period_to_start_date(self, period: str) -> datetime:
+        :param period: Periodo di coverage richiesto al provider.
+        :type period: str
+        :return: Data di inizio corrispondente al periodo richiesto.
+        :rtype: datetime | None
+        """
         period = period.lower()
         today = pd.Timestamp.today()
 
-        if period.endswith("y"):
+        # NOTE: per i giorni si usa pd.Timedelta, mentre per mesi e anni si usa pd.DateOffset.
+        # NOTE: Questo perché pd.DateOffset gestisce correttamente mesi/anni (inclusi anni bisestili),
+        # NOTE: mentre pd.Timedelta è adatto solo per intervalli di giorni.
+        if period == 'max':
+            return None
+        elif period.endswith("y"):
             years = int(period[:-1])
             return today - pd.DateOffset(years=years)
-
         elif period.endswith("mo"):
             months = int(period[:-2])
             return today - pd.DateOffset(months=months)
-
         elif period.endswith("d"):
             days = int(period[:-1])
             return today - pd.Timedelta(days=days)
-
         else:
             raise ValueError(f"Invalid period format: {period}")
